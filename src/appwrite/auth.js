ec6ee6a91a196a9deb2956185c6df58de07631c7
Aditya Sharma <3008.aditya.sharma@gmail.com>
2024-10-20 12:06:55 +0530

    auth commit

---
+++ src/appwrite/auth.js
@@ -0,0 +1,55 @@
+import { Account, Client, ID } from "appwrite";
+import conf from "../conf/conf";
+
+export class AuthService {
+	client = new Client();
+	account;
+
+	constructor() {
+		this.client
+			.setEndpoint(conf.appwriteUrl)
+			.setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+	}
+
+    async createAccount({email, password, name}) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                this.login({email, password});
+            } else return userAccount;
+        } catch(error) {
+            console.log("Appwrite serive :: createAccount :: error", error);
+        }
+    }
+
+    async login({email, password}) {
+        try {
+            return await this.account.createEmailPasswordSession(email, password);
+        } catch (error) {
+            console.log("Appwrite serive :: login :: error", error);
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return this.account.get();
+        } catch (error) {
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
+        }
+        
+        return null;
+    }
+    
+    async logout() {
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite serive :: logout :: error", error);
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService;
